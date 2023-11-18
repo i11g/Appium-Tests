004da7e69b5b748e86a707ca382c1bedbbd153b3
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2023-11-18 14:59:22 +0200

    Add project files.

---
+++ A miner Task/Program.cs
@@ -0,0 +1,23 @@
+﻿string resource = Console.ReadLine();
+
+Dictionary<string,int> resourses= new Dictionary<string,int>(); 
+
+while(resource!="stop")
+{
+    int quantity=int.Parse(Console.ReadLine()); 
+
+    if(resourses.ContainsKey(resource))
+    {
+        resourses[resource] += quantity;
+    }
+    else
+    {
+        resourses.Add(resource, quantity);
+    }
+    resource= Console.ReadLine();
+}
+
+foreach (var res in resourses)
+{
+    Console.WriteLine($"{res.Key} -> {res.Value}");
+}
---
+++ Orders/Program.cs
@@ -0,0 +1,35 @@
+﻿string products = Console.ReadLine();
+
+Dictionary<string, List<double>>  items = new Dictionary<string, List<double>>();
+
+while (products!="buy")
+{
+    List<string> products1 = products.Split(' ').ToList();
+    string product = products1[0];
+    double price = double.Parse(products1[1]);
+    double quantity = double.Parse(products1[2]);
+    
+   if (!items.ContainsKey(product))
+    {
+        items.Add(product,new List<double>()); 
+        items[product].Add(price);
+        items[product].Add(quantity);
+
+     }
+    else
+    {
+        items[product][0] = price;
+        items[product][1] += quantity;
+    }
+    products = Console.ReadLine();
+}
+foreach (KeyValuePair<string, List<double>> currentproduct in items)
+{
+    string currentProductName = currentproduct.Key;
+    double currentProductPrice = currentproduct.Value[0];
+    double currentProductQuantity = currentproduct.Value[1];
+
+    double totalPrice=currentProductPrice*currentProductQuantity;
+
+    Console.WriteLine($"{currentProductName} -> {totalPrice:f2}");
+}
