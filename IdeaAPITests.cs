1ac41276c5bd8ce315da1ea08e8cd0d28cf57c15
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-04-14 09:16:30 +0300

    new files added

---
+++ IdeaAPITests.cs
@@ -0,0 +1,16 @@
+namespace IdeaAPI.Tests
+{
+    public class IdeaAPITests
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
