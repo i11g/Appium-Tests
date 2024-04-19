89173e63a21eb0cc2fb3456466ea58732f849ee9
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-04-19 17:10:23 +0300

    files added

---
+++ IdeaAPITesting/Test1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //for (int j = 0; j < i; j++)
+                //{
+                //    Console.WriteLine(j);
+                //}   
+
+                Console.Write(i);
+            }
+        }
+    }
+}
